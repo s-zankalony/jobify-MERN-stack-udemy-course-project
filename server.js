ccfbb756248f2205faa96df1f508aebd401ada89
Sameh El Zankalony <zankalony@gmail.com>
2024-01-14 22:42:34 +0200

    test commit

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import express from 'express';
 const app = express();
 
+// middleware
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello, World');
 });
+
 
 app.post('/', (req, res) => {
   console.log(req);
