d58d25894ea40b5bee2bc648e6b0242c50cf3f7b
Sameh El Zankalony <zankalony@gmail.com>
2024-02-29 14:39:39 +0200

    finished stats page, starting in alljobs search

--- client/src/components/ChartsContainer.jsx
+++ client/src/components/ChartsContainer.jsx
@@ -1,5 +1,21 @@
-const ChartsContainer = () => {
-  return <h1>ChartsContainer</h1>;
+import { useState } from 'react';
+
+import BarChart from './BarChart';
+import AreaChartComponent from './AreaChart';
+import Wrapper from '../assets/wrappers/ChartsContainer';
+
+const ChartsContainer = ({ data }) => {
+  const [barChart, setBarChart] = useState(true);
+
+  return (
+    <Wrapper>
+      <h4>Monthly Applications</h4>
+      <button type="button" onClick={() => setBarChart(!barChart)}>
+        {barChart ? 'Area Chart' : 'Bar Chart'}
+      </button>
+      {barChart ? <BarChart data={data} /> : <AreaChartComponent data={data} />}
+    </Wrapper>
+  );
 };
 
 export default ChartsContainer;
