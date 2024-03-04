3f1d76be34d8190eda2caf7d268558599ff6532c
Sameh El Zankalony <zankalony@gmail.com>
2024-03-04 23:34:05 +0200

    completed search functionality and starting to implement pagination

--- client/src/components/PageBtnContainer.jsx
+++ client/src/components/PageBtnContainer.jsx
@@ -10,113 +10,33 @@ const PageBtnContainer = () => {
   const pages = Array.from({ length: numOfPages }, (_, index) => {
     return index + 1;
   });
-
-  const { search, pathname } = useLocation();
-  const navigate = useNavigate();
-
-  const handlePageChange = (pageNumber) => {
-    const searchParams = new URLSearchParams(search);
-    searchParams.set('page', pageNumber);
-    navigate(`${pathname}?${searchParams.toString()}`);
-  };
-
-  const addPageButton = ({ pageNumber, activeClass }) => {
-    return (
-      <button
-        className={`btn page-btn ${activeClass && 'active'}`}
-        key={pageNumber}
-        onClick={() => handlePageChange(pageNumber)}
-      >
-        {pageNumber}
-      </button>
-    );
-  };
-
-  const renderPageButtons = () => {
-    const pageButtons = [];
-    // first page
-    pageButtons.push(
-      addPageButton({ pageNumber: 1, activeClass: currentPage === 1 })
-    );
-    // dots
-
-    if (currentPage > 3) {
-      pageButtons.push(
-        <span className='page-btn dots' key='dots-1'>
-          ...
-        </span>
-      );
-    }
-    // one before current page
-    if (currentPage !== 1 && currentPage !== 2) {
-      pageButtons.push(
-        addPageButton({
-          pageNumber: currentPage - 1,
-          activeClass: false,
-        })
-      );
-    }
-    // current page
-    if (currentPage !== 1 && currentPage !== numOfPages) {
-      pageButtons.push(
-        addPageButton({
-          pageNumber: currentPage,
-          activeClass: true,
-        })
-      );
-    }
-    // one after current page
-
-    if (currentPage !== numOfPages && currentPage !== numOfPages - 1) {
-      pageButtons.push(
-        addPageButton({
-          pageNumber: currentPage + 1,
-          activeClass: false,
-        })
-      );
-    }
-    if (currentPage < numOfPages - 2) {
-      pageButtons.push(
-        <span className='page-btn dots' key='dots+1'>
-          ...
-        </span>
-      );
-    }
-    pageButtons.push(
-      addPageButton({
-        pageNumber: numOfPages,
-        activeClass: currentPage === numOfPages,
-      })
-    );
-    return pageButtons;
-  };
-
+  console.log(pages);
   return (
     <Wrapper>
-      <button
-        className='btn prev-btn'
-        onClick={() => {
-          let prevPage = currentPage - 1;
-          if (prevPage < 1) prevPage = numOfPages;
-          handlePageChange(prevPage);
-        }}
-      >
+      <button className="btn prev-btn">
         <HiChevronDoubleLeft />
         prev
       </button>
-      <div className='btn-container'>{renderPageButtons()}</div>
-      <button
-        className='btn next-btn'
-        onClick={() => {
-          let nextPage = currentPage + 1;
-          if (nextPage > numOfPages) nextPage = 1;
-          handlePageChange(nextPage);
-        }}
-      >
-        next
+      <div className="btn-container">
+        {pages.map((pageNumber) => {
+          return (
+            <button
+              className={`btn page-btn ${
+                pageNumber === currentPage && 'active'
+              }`}
+              key={pageNumber}
+            >
+              {pageNumber}
+            </button>
+          );
+        })}
+      </div>
+      <button className="btn next-btn">
         <HiChevronDoubleRight />
+        next
       </button>
     </Wrapper>
   );
 };
+
 export default PageBtnContainer;
