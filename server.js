d23dd6a0f35ddf13d8be9fdc6cdb00541d632ec8
Sameh El Zankalony <zankalony@gmail.com>
2024-03-06 22:50:38 +0200

    deployment version

--- server.js
+++ server.js
@@ -58,6 +58,10 @@ app.use('/api/v1/auth', authRouter);
 // app.patch('/api/v1/jobs/:id', updateJob);
 // app.delete('/api/v1/jobs/:id', deleteJob);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './public', 'index.html'));
+});
+
 app.use('*', (req, res) => {
   res.status(404).json({ msg: 'not found' });
 });
